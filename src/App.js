9075a1b472f55e37549f8e3d435c35616ed9d5f2
navneet2508 <navneety250801@gmail.com>
2022-11-09 15:53:00 +0530

    Converted from class based to function based component

--- src/App.js
+++ src/App.js
@@ -1,134 +1,129 @@
 import "./App.css";
 
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Navbar from "./Components/Navbar";
 import News from "./Components/News";
 import { Routes, Route } from "react-router-dom";
 import LoadingBar from "react-top-loading-bar";
 
-export default class App extends Component {
+const App = () => {
   // c='Navneet'
-  pageSize = 5;
-  apiKey = process.env.REACT_APP_NEWS_API;
-  state = {
-    progress: 0,
-  };
+  const pageSize = 5;
+  const apikey = process.env.REACT_APP_NEWS_API;
 
-  setProgress = (progress) => {
-    this.setState({ progress: progress });
-  };
-  render() {
-    return (
-      <div>
-        <Navbar />
-        <LoadingBar height={3} color="#f11946" progress={this.state.progress} />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <News
-                setProgress={this.setProgress}
-                apikey={this.apiKey}
-                key="general"
-                pageSize={this.pageSize}
-                country="in"
-                category="general"
-              />
-            }
-          />
-          <Route
-            path="/business"
-            element={
-              <News
-                setProgress={this.setProgress}
-                apikey={this.apiKey}
-                key="business"
-                pageSize={this.pageSize}
-                country="in"
-                category="business"
-              />
-            }
-          />
-          <Route
-            path="/entertainment"
-            element={
-              <News
-                setProgress={this.setProgress}
-                apikey={this.apiKey}
-                key="entertainment"
-                pageSize={this.pageSize}
-                country="in"
-                category="entertainment"
-              />
-            }
-          />
-          <Route
-            path="/general"
-            element={
-              <News
-                setProgress={this.setProgress}
-                apikey={this.apiKey}
-                key="general"
-                pageSize={this.pageSize}
-                country="in"
-                category="general"
-              />
-            }
-          />
-          <Route
-            path="/health"
-            element={
-              <News
-                setProgress={this.setProgress}
-                apikey={this.apiKey}
-                key="health"
-                pageSize={this.pageSize}
-                country="in"
-                category="health"
-              />
-            }
-          />
-          <Route
-            path="/science"
-            element={
-              <News
-                setProgress={this.setProgress}
-                apikey={this.apiKey}
-                key="science"
-                pageSize={this.pageSize}
-                country="in"
-                category="science"
-              />
-            }
-          />
-          <Route
-            path="/sports"
-            element={
-              <News
-                setProgress={this.setProgress}
-                apikey={this.apiKey}
-                key="sports"
-                pageSize={this.pageSize}
-                country="in"
-                category="sports"
-              />
-            }
-          />
-          <Route
-            path="/technology"
-            element={
-              <News
-                setProgress={this.setProgress}
-                apikey={this.apiKey}
-                key="technology"
-                pageSize={this.pageSize}
-                country="in"
-                category="technology"
-              />
-            }
-          />
-        </Routes>
-      </div>
-    );
-  }
-}
+  const [progress, setProgress] = useState(0);
+
+  return (
+    <div>
+      <Navbar />
+      <LoadingBar height={3} color="#f11946" progress={progress} />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <News
+              setProgress={setProgress}
+              apikey={apikey}
+              key="general"
+              pageSize={pageSize}
+              country="in"
+              category="general"
+            />
+          }
+        />
+        <Route
+          path="/business"
+          element={
+            <News
+              setProgress={setProgress}
+              apikey={apikey}
+              key="business"
+              pageSize={pageSize}
+              country="in"
+              category="business"
+            />
+          }
+        />
+        <Route
+          path="/entertainment"
+          element={
+            <News
+              setProgress={setProgress}
+              apikey={apikey}
+              key="entertainment"
+              pageSize={pageSize}
+              country="in"
+              category="entertainment"
+            />
+          }
+        />
+        <Route
+          path="/general"
+          element={
+            <News
+              setProgress={setProgress}
+              apikey={apikey}
+              key="general"
+              pageSize={pageSize}
+              country="in"
+              category="general"
+            />
+          }
+        />
+        <Route
+          path="/health"
+          element={
+            <News
+              setProgress={setProgress}
+              apikey={apikey}
+              key="health"
+              pageSize={pageSize}
+              country="in"
+              category="health"
+            />
+          }
+        />
+        <Route
+          path="/science"
+          element={
+            <News
+              setProgress={setProgress}
+              apikey={apikey}
+              key="science"
+              pageSize={pageSize}
+              country="in"
+              category="science"
+            />
+          }
+        />
+        <Route
+          path="/sports"
+          element={
+            <News
+              setProgress={setProgress}
+              apikey={apikey}
+              key="sports"
+              pageSize={pageSize}
+              country="in"
+              category="sports"
+            />
+          }
+        />
+        <Route
+          path="/technology"
+          element={
+            <News
+              setProgress={setProgress}
+              apikey={apikey}
+              key="technology"
+              pageSize={pageSize}
+              country="in"
+              category="technology"
+            />
+          }
+        />
+      </Routes>
+    </div>
+  );
+};
+export default App;
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,53 +1,50 @@
-import React, { Component } from "react";
+import React from "react";
 
-export class NewsItem extends Component {
-  render() {
-    let { title, description, imageUrl, newsUrl, author, date, source } =
-      this.props;
-    console.log("props", this.props);
-    console.log("source info: ", source);
-    // debugger;
-    return (
-      <div className="my-3">
-        <div className="card">
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "flex-end",
-              position: "absolute",
-              right: "0",
-            }}
+const NewsItem = (props) => {
+  let { title, description, imageUrl, newsUrl, author, date, source } = props;
+  console.log("props", props);
+  console.log("source info: ", source);
+  // debugger;
+  return (
+    <div className="my-3">
+      <div className="card">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            position: "absolute",
+            right: "0",
+          }}
+        >
+          <span
+            class="badge rounded-pill bg-danger"
+            style={{ left: "90%", zIndex: "1" }}
           >
-            <span
-              class="badge rounded-pill bg-danger"
-              style={{ left: "90%", zIndex: "1" }}
-            >
-              {source}
-            </span>
-          </div>
-          <img src={imageUrl} className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">{title} </h5>
-            <p className="card-text">{description}</p>
-            <p className="card-text">
-              <small className="text-muted">
-                By {!author ? "Unknown" : author} on
-                {new Date(date).toGMTString()}
-              </small>
-            </p>
-            <a
-              rel="noreferrer"
-              href={newsUrl}
-              target="_blank"
-              className="btn btn-sm btn-primary"
-            >
-              Read More
-            </a>
-          </div>
+            {source}
+          </span>
+        </div>
+        <img src={imageUrl} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{title} </h5>
+          <p className="card-text">{description}</p>
+          <p className="card-text">
+            <small className="text-muted">
+              By {!author ? "Unknown" : author} on
+              {new Date(date).toGMTString()}
+            </small>
+          </p>
+          <a
+            rel="noreferrer"
+            href={newsUrl}
+            target="_blank"
+            className="btn btn-sm btn-primary"
+          >
+            Read More
+          </a>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default NewsItem;
